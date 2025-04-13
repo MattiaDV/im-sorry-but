37d69a599acc8475f6259bd0b00d1b1394cf36c0
MattiaDV <mattiadv25105@gmail.com>
2025-04-14 01:00:49 +0200

    a

--- excuse.js
+++ excuse.js
@@ -1,5 +1,11 @@
 export let excuse = [
     "my dog eat the router!",
     "my mom say no!",
-    "my pc is dead"
+    "my pc is dead",
+    "my brain is not braining",
+    "for me it's too late",
+    "today my parrot have diarreah",
+    "my girlfriend say no",
+    "my boyfriend say no",
+    "my mother say to me 'you are so special, you can't move alone'"
 ]
